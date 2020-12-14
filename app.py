8d0108cbe7cababa93479ba9d47172ea397e148d
linus3003 <56354804+linus3003@users.noreply.github.com>
2020-12-14 23:55:07 +0100

    requirements added

--- app.py
+++ app.py
@@ -4,22 +4,29 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
+from numpy import random
+from dash.dependencies import Input, Output
 
-#import json
-#import base64
-#import datetime
-#import requests
-#import pathlib
-#import math
 
 external_stylesheets = [dbc.themes.FLATLY]
 
 df = pd.read_csv('resources/alldata.csv', index_col=0)
 df = df[df['price'].notna()]
+df = df[df['sqft'].notna()]
 
 # remove invalid points outside Germany
 df = df.loc[(df['longitude'] <= 15.58) & (df['longitude'] >= 5.58)]
 df = df.loc[(df['latitude'] <= 55.05) & (df['latitude'] >= 47.25)]
+
+#insert slight variation to longitude/latitude to display units of same adress
+
+df['longitude'] =[x + random.choice([i for i in range(-10,10) if i != 0])/1000000 for x in df['longitude']]
+df['latitude'] =[x + random.choice([i for i in range(-10,10) if i != 0])/1000000 for x in df['latitude']]
+
+#create scale for size of markers
+df_diffq = (df["price"].max() - df["price"].min()) / 16
+df["scale"] = (df["price"] - df["price"].min()) / df_diffq + 1
+
 
 # add navbar
 navbar = dbc.NavbarSimple(
@@ -43,8 +50,8 @@ navbar = dbc.NavbarSimple(
 
 )
 
-#interactive map
-fig = px.scatter_mapbox(df, lat="latitude", lon="longitude", color="price", size="price", size_max=15, hover_name="url",
+#interactive ma
+fig = px.scatter_mapbox(df, lat="latitude", lon="longitude", color="sqft", hover_name="url", size = df['scale'], size_max=15,
                         color_continuous_scale=px.colors.diverging.Portland, zoom=4, mapbox_style='carto-positron',
                         opacity=1)
 # px.colors.diverging.Portland , px.colors.cyclical.IceFire
@@ -61,14 +68,14 @@ hist = px.histogram(df, x="price",
                     )
 
 hist.update_layout(
-    margin=dict(l=0, r=25, t=50, b=0),
+    margin=dict(l=0, r=0, t=50, b=0),
     paper_bgcolor='rgba(237,240,241,0.8)',
     plot_bgcolor='rgba(237,240,241,0.8)'
 )
 
 # styles: open-street-map, white-bg, carto-positron, carto-darkmatter, stamen-terrain, stamen-toner, stamen-watercolor
 
-YEARS = [2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030]
+YEARS = [2020, 2021, 2022, 2023, 2024, 2025]
 
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])  # , external_stylesheets=external_stylesheets)
@@ -151,7 +158,7 @@ app.layout = html.Div(children=[
                             )
                         ]
                     )
-                ], style={'display': 'inline-block', 'width': '75%'}
+                ], style={'display': 'inline-block', 'width': '70%'}
             ),
 
         ], style={'marginBottom': 25, 'marginTop': 25, 'marginLeft': 25, 'marginRight': 25}
@@ -163,10 +170,46 @@ app.layout = html.Div(children=[
             dcc.Graph(id='map', figure=fig, style={'display': 'inline-block', 'width': '75%'}),
             dcc.Graph(id='histogram', figure=hist, style={'display': 'inline-block', 'width': '25%'})
         ]
-    )
+    ),
 
 ])
 
+
+@app.callback(
+    Output('map', 'figure'),
+    Input('years-slider', 'value'))
+
+
+def update_figure(selected_year):
+
+    f = "Fertigstellung "
+
+    if (selected_year==2024):
+        filtered_df = df[df.dev_status.isin([f + "2020", "Fertiggestellt", f + "2021",
+                                              f + "2022", f + "2023", f + "2024"])]
+    elif (selected_year==2023):
+        filtered_df = df[df.dev_status.isin([f + "2020", "Fertiggestellt", f + "2021",
+                                               f + "2022", f + "2023"])]
+    elif (selected_year == 2022):
+        filtered_df = df[df.dev_status.isin([f + "2020", "Fertiggestellt", f + "2021",
+                                               f + "2022"])]
+    elif (selected_year == 2021):
+        filtered_df = df[df.dev_status.isin([f + "2020", "Fertiggestellt", f + "2021"])]
+    elif (selected_year == 2020):
+        filtered_df = df[df.dev_status.isin([f + "2020", "Fertiggestellt"])]
+
+    else: filtered_df=df
+
+    fig = px.scatter_mapbox(filtered_df, lat="latitude", lon="longitude", color="sqft", hover_name="url", size=filtered_df['scale'],
+                            size_max=15,
+                            color_continuous_scale=px.colors.diverging.Portland, zoom=4, mapbox_style='carto-positron',
+                            opacity=1)
+
+    fig.update_layout(transition_duration=500,
+
+                      margin=dict(l=0, r=0, t=0, b=0))
+
+    return fig
 
 
 if __name__ == '__main__':
